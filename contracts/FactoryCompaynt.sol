2e5bc31ce33038e467a7df68b20c9498bbf59c36
chandu8817 <virendrakapoor45@gmail.com>
2023-04-25 16:48:55 +0530

    add the company factorty

---
+++ contracts/FactoryCompaynt.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+import "./Company.sol";
+
+contract CompanyFactory{
+    Company[] poblic companies;
+    function createCompany(address _user,string memory _name)external payable{
+
+    }
+}
