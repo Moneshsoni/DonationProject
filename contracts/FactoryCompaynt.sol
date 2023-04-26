66dc0f25e7f06f85238a409e634824860a919652
Moneshsoni <sonimonesh115@gmail.com>
2023-04-26 19:30:59 +0530

    add the factory contract

--- contracts/FactoryCompaynt.sol
+++ contracts/FactoryCompaynt.sol
@@ -5,6 +5,16 @@ import "./Company.sol";
 contract CompanyFactory{
     Company[] public companies;
     function createCompany(address _user,string memory _name)external payable{
-        
+        Company company = new Company(_user,_name);
+        companies.push(company);
     }
+
+    function getCompaniesLenth()public view returns(uint){
+        return companies.length;
+    }
+
+    function getCompanies()public view returns(Company[] memory){
+        return companies;
+    }
+
 }
