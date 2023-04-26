f35e4b0dbbd92b969181c137e708f00957ba25db
Moneshsoni <sonimonesh115@gmail.com>
2023-04-26 15:11:46 +0530

    add the upgradable contracts

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,15 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
-contract Escrow{
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract Escrow is Initializable, UUPSUpgradeable, OwnableUpgradeable{
     address public buyer;
     address payable seller;
-    enum ComState{Start,Deliver,Coplete}
+    enum ComState{Start,Deliver,Complete}
     ComState public state;
 
-    constructor(address _buyer, address payable _seller){
+    function initialize(address _buyer, address payable _seller)public initializer{
         buyer = _buyer;
         seller = _seller;
+        __Ownable_init();
     }
+
+    ///@dev required by the OZ UUPS module
+    function _authorizeUpgrade(address) internal override onlyOwner {}
 
     modifier onlyBuyer(){
         require(msg.sender == buyer,"Only owner can Call");
@@ -24,6 +32,13 @@ contract Escrow{
     function ReleaseFund()external onlyBuyer{
         require(state==ComState.Deliver,"This is not Deliver yer");
         seller.transfer(address(this).balance);
-        state = ComState.Coplete;
+        state = ComState.Complete;
+    }
+
+    function add(uint a,uint b)public pure returns(uint){
+        return a+b;
     }
 }
+
+// buyer 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
+// seller 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db
