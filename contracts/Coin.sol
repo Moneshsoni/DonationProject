5de51c5ce79b6b3105d02a7d66921886dfb1e1f1
Moneshsoni <sonimonesh115@gmail.com>
2023-04-27 10:08:32 +0530

    add The coin contract

--- contracts/Coin.sol
+++ contracts/Coin.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 contract Coin{
+    uint public totalSupply;
+    uint public totalAmount;
     mapping(address => uint)public balances;
     address public owner;
     constructor(){
@@ -12,8 +14,15 @@ contract Coin{
         _;
     }
 
+    function setMaxSupply(uint _totalSupply)public{
+        totalSupply = _totalSupply;
+    }
+
     function mint(address receiver,uint amount)public OnlyOwner{
+        require(amount<=totalSupply,"You can't mint greater than maxSupply greater than maxsupply");
+        require(totalAmount<totalSupply,"You can't mint more than totalSupply");
         balances[receiver] += amount;
+        totalAmount += amount;
     }
 
 
